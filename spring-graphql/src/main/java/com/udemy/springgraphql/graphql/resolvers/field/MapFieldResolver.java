16b9a69f76b9ce01746255989762e6e3256e9bb7
Tomas <tomasoares@gmail.com>
2022-04-15 13:56:08 -0300

    lesson 28 - Added review reference to maps, and added respective method to MapFieldResolver

--- spring-graphql/src/main/java/com/udemy/springgraphql/graphql/resolvers/field/MapFieldResolver.java
+++ spring-graphql/src/main/java/com/udemy/springgraphql/graphql/resolvers/field/MapFieldResolver.java
@@ -2,10 +2,15 @@ package com.udemy.springgraphql.graphql.resolvers.field;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.udemy.springgraphql.graphql.type.Map;
+import com.udemy.springgraphql.graphql.type.Review;
 import com.udemy.springgraphql.graphql.type.Wad;
 import com.udemy.springgraphql.service.WadService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 @Log4j2
@@ -24,6 +29,11 @@ public class MapFieldResolver implements GraphQLResolver<Map> {
 
         log.info("Map found: {}", result);
         return result;
+    }
+
+    public List<Review> reviews(Map map, Integer count) {
+        log.info("Retrieving reviews of map {}", map.getId());
+        return List.of(Review.builder().id(UUID.randomUUID()).build());
     }
 
 }
