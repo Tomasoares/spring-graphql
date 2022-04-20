747130069f5d397827c9c60d5242867fe2f09b7a
Tomas <tomasoares@gmail.com>
2022-04-20 19:14:11 -0300

    lesson 41 - moved converters methods to its respective own class

---
+++ spring-graphql/src/main/java/com/udemy/springgraphql/jpa/mapper/WadMapper.java
@@ -0,0 +1,34 @@
+package com.udemy.springgraphql.mapper;
+
+import com.udemy.springgraphql.graphql.type.Wad;
+import com.udemy.springgraphql.graphql.type.WadInput;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WadMapper {
+
+    public static List<Wad> convertList(final List<com.udemy.springgraphql.jpa.model.Wad> fromDb) {
+        return fromDb
+                .stream()
+                .map(this::toGraphQLWad)
+                .collect(Collectors.toList());
+    }
+
+    public static Wad toGraphQLWad(final com.udemy.springgraphql.jpa.model.Wad wad) {
+        return Wad.builder()
+                .id(wad.getId())
+                .name(wad.getName())
+                .genre(wad.getGenre())
+                .iwad(wad.getIwad())
+                .build();
+    }
+
+    public static com.udemy.springgraphql.jpa.model.Wad toJPA(final WadInput input) {
+        return com.udemy.springgraphql.jpa.model.Wad.builder()
+                .name(input.getName())
+                .genre(input.getGenre())
+                .iwad(input.getIwad())
+                .build();
+    }
+}
