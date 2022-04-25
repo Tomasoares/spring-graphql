73fd5f2618a5cdca2744b3d8f6e9c5c8c1597765
Tomas <tomasoares@gmail.com>
2022-04-25 11:08:17 -0300

    lesson 53 - Added async request for concurrent Review nested fields

--- spring-graphql/src/main/java/com/udemy/springgraphql/graphql/resolver/field/ReviewFieldResolver.java
+++ spring-graphql/src/main/java/com/udemy/springgraphql/graphql/resolver/field/ReviewFieldResolver.java
@@ -9,6 +9,11 @@ import graphql.kickstart.tools.GraphQLResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 @Slf4j
@@ -18,20 +23,29 @@ public class ReviewFieldResolver implements GraphQLResolver<Review> {
     private final WadService wadService;
     private final MapService mapService;
 
-    public Wad getWad(final Review review) {
-        log.info("Finding wad of review: {}", review);
-        final var wad = this.wadService.findWadByReview(review.getId());
+    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+    public CompletableFuture<Wad> getWad(final Review review) {
+        log.info("Finding wad of review: {}", review);
+        return CompletableFuture.supplyAsync(() -> findWadById(review));
+    }
+
+    private Wad findWadById(Review review) {
+        final var wad = this.wadService.findWadByReview(review.getId());
         log.info("Found the following wad: {}", wad);
+
         return wad;
     }
 
-    public Map getMap(final Review review) {
+    public CompletableFuture<Map> getMap(final Review review) {
         log.info("Finding map of review: {}", review);
+        return CompletableFuture.supplyAsync(() -> this.findMapByReview(review));
+    }
 
+    private Map findMapByReview(Review review) {
         final var map = this.mapService.findMapByReview(review.getId());
-
         log.info("Found the following map: {}", map);
+
         return map;
     }
 }
